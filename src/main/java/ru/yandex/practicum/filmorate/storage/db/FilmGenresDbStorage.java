54cc8b665b0e5208cbf9dd2cd2cd8db0826e6abf
kiselevm2023 <kiselevm2023@yandex.ru>
2024-01-17 01:08:27 +0400

    ТЗ-11 семнадцатая редакция

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/FilmGenresDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/FilmGenresDbStorage.java
@@ -47,6 +47,7 @@ public class FilmGenresDbStorage implements FilmGenresStorage {
         log.warn("Adding a list of movie genres to the database: " + film);
         List<Genre> genres = new ArrayList<>(film.getGenres());
         jdbcTemplate.batchUpdate(INSERT_GENRES_BY_FILM , new BatchPreparedStatementSetter() {
+
             @Override
             public void setValues(@NonNull PreparedStatement ps, int i) throws SQLException {
                 ps.setInt(1, genres.get(i).getId());
